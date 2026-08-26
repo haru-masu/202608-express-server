7851c5841e191c2edf7c6b1667deab01bc5b750e
haru-masu <yuuto.5853649@gmail.com>
2026-08-26 17:22:30 +0900

    fix: Express EJS課題完成

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 import express, { Request, Response } from 'express';
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
 
-//ルーティング /
+app.use(express.json()); //JSON形式のデータ をサーバー側で読み取れるようにする
+app.use(express.static("./public")); //静的ファイルの指定
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, Express + TypeScript World!');
 });
 
-//ルーティングを追加
+
 app.get('/test', (req: Request, res: Response) => {
   const data = {
-    'title' : 'タイトル',
-    'lists': ['リスト1','リスト2','リスト2','リスト2']
-  }
-  res.render('test.ejs',data);
+    'title': 'タイトル',
+    'lists': ['リスト1', 'リスト2', 'リスト2', 'リスト2']
+  };
+
+  res.render('test.ejs', data);
 });
 
 
